93ed7ebe59655dfdac8e4a7c428746bb4dc2c041
IVIS16 공용계정 <O1433@mn-soft.co.kr>
2019-12-05 13:05:56 +0900

    -현재 코드 수정중입니다.

--- pdkFileManagement_02/mainwindow.cpp
+++ pdkFileManagement_02/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QLabel>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -17,7 +19,12 @@ void MainWindow::on_pushButton_add_clicked()
 {
     QLabel *label = new QLabel();
     label->setText(ui->lineEdit->text());
+
+    //////////  Jo /////////
     ui->tableWidget->setCellWidget(0, 0, label);
+    if(QString::compare(ui->lineEdit->text(),"") != 0){
+
+    }
 }
 
 void MainWindow::on_pushButton_quit_clicked()
