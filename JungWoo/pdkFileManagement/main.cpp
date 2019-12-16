f1159bfc99f1c792b19f38b761ad776863ea04cd
JungWoo <dlawjddnrj01@github.com>
2019-12-16 17:13:13 +0900

    임정우 현재 add 버튼 누를 시 .txt 파일에 덮어쓰기 가능 (Append) load 버튼 누르면 테이블 위젯에 있는 row에 .txt에 있는 값들을 화면 상에 표출 해줘야 함.

--- JungWoo/pdkFileManagement/main.cpp
+++ JungWoo/pdkFileManagement/main.cpp
@@ -1,54 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QFile>
-#include <QString>
-#include <QDebug>
-#include <QTextStream>
-
-void Write(QString Filename)
-{
-    QFile mFile(Filename);
-
-    if (!mFile.open(QFile::WriteOnly | QFile::Text))
-    {
-        qDebug() << "could not open file for writing";
-        return;
-    }
-    QTextStream out(&mFile);
-    out << "Hello World";
-
-    mFile.flush();
-    mFile.close();
-}
-
-void Read(QString Filename)
-{
-    QFile mFile(Filename);
-
-    if (!mFile.open(QFile::ReadOnly | QFile::Text))
-    {
-        qDebug() << "could not open file for reading";
-        return;
-    }
-    QTextStream in(&mFile);
-    QString mText = in.readAll();
-
-    qDebug() << mText;
-
-    mFile.flush();
-    mFile.close();
-}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-    QString mFilename = "C:/Users/ghktk/Desktop/pdkFileManagement_01/pdklist.txt";
-
-    Write(mFilename);
-    Read(mFilename);
 
     return a.exec();
 }
--- JungWoo/pdkFileManagement/mainwindow.cpp
+++ JungWoo/pdkFileManagement/mainwindow.cpp
@@ -1,18 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QFile>
-
-#include <QDir>
-#include <QLabel>
+#include <QString>
 #include <QTextStream>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),        //  Qt::FramelessWindowHint를 parent, 옆에 쓰면 타이틀이 사라짐
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QString style = "color: red;";
-    ui->pushButton_quit->setStyleSheet(style);
+    QString addStyle = "color: blue";
+    QString loadStyle = "color: pink";
+    QString removeStyle = "color: red";
+    QString quitStyle = "color: yellow";
+
+    ui->pushButton_add->setStyleSheet(addStyle);
+    ui->pushButton_load->setStyleSheet(loadStyle);
+    ui->pushButton_remove->setStyleSheet(removeStyle);
+    ui->pushButton_quit->setStyleSheet(quitStyle);
 }
 
 MainWindow::~MainWindow()
@@ -22,9 +28,8 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_add_clicked()
 {
-    // 텍스트 리스트 뷰
-    QLabel *label = new QLabel();
     QTableWidgetItem *tableItem = new QTableWidgetItem();
+    QString mFilename = "C:/Git_Group_Server/JungWoo/pdkFileManagement/pdklist.txt";
 
     tableItem->setText(ui->lineEdit->text());
 
@@ -34,12 +39,23 @@ void MainWindow::on_pushButton_add_clicked()
             ui->tableWidget->setItem(i, 0, tableItem);
         }
     }
-//    if(label->text() != "") {                     // 수정 안되는 코드
-//        for(int i = 0; i < 1; i++) {
-//            ui->tableWidget->insertRow(i);
-//            ui->tableWidget->setCellWidget(i, 0, label);      setCellWidget으로 텍스트를 추가하면서 넣었기 때문에 row에 대한 수정이 불가능
-//        }
-//    }
+
+    QFile mFile(mFilename);
+
+    QTextStream out(&mFile);
+
+    if (!mFile.open(QFile::WriteOnly | QFile::Append | QFile::Text))
+    {
+        qDebug() << "write!!";
+        return;
+    }
+
+    int index = tableItem->row();
+    QString userInput = ui->tableWidget->item(index, 0)->text();
+    out << userInput << endl;
+
+    mFile.flush();
+    mFile.close();
 }
 
 void MainWindow::on_pushButton_quit_clicked()
@@ -52,62 +68,22 @@ void MainWindow::on_pushButton_remove_clicked()
     ui->tableWidget->removeRow(0);
 }
 
-void MainWindow::on_pushButton_save_clicked()
+void MainWindow::on_pushButton_load_clicked()
 {
-//    QFile file("C:\\Git_Group_Server\\pdkFileManagement_02\\pdklist.txt");
-//    file.open(QIODevice::WriteOnly);
-//    QDataStream out (&file);
-//    QString str = textEdit->toPlainText(); // tableWidget 중 row에 저장된 text 값을 str에 저장
-//    // QStringlist 이걸로 바꿔야돼 Item들을
+    QFile mFile("pdklist.txt");
 
-//    out.setVersion(QDataStream::Qt_5_12);
-
-//    QString ApplicationPath = QApplication::applicationDirPath();
-//    QFile File(ApplicationPath + "C:\\Git_Group_Server\\pdkFileManagement_01\\pdklist.txt");
-
-//    QDir Directory("test.txt");
-
-//    // 파일 읽기
-//    if(!File.open(QFile::ReadOnly | QFile::Text)) {     // 읽기 전용, 텍스트로 파일 열기
-//        if(!File.exists()) {        // 파일이 존재하지 않으면..
-//            Directory.mkdir("C:\\Git_Group_Server\\pdkFileManagement_01");
-//        } else {
-//            close();
-//        }
-//    }
-
-//    QTextStream OpenFile(&File);
-//    QString ConfigText;
-
-//    while(!OpenFile.atEnd()) {      // 파일 끝까지 읽어서
-//        ConfigText = OpenFile.readLine();   // 한 라인씩 읽어서 변수에 적용
-//    }
-
-//    File.close();
-
-//    // 파일 쓰기
-//    File.open(QFile::WriteOnly | QFile::Append | QFile::Text);
-//    QTextStream SaveFile(&File);
-//    SaveStream << "abc" << "\n";
-//    SaveStream << "123" << "\n";
-
-//    File.close();
-    QFile *file = new QFile;
-
-    QString filename = "sample.txt";
-
-    file->setFileName(QApplication::applicationDirPath() + "/" + filename);
-
-    if (!file->open(QIODevice::WriteOnly))
+    if (!mFile.open(QIODevice::ReadOnly | QIODevice::Text))
     {
-        if (!file->exists())
-        {
-
-        }
+        qDebug() << "read!!";
+        return;
     }
+    QTextStream in(&mFile);
 
-    QString str("sample Text \n");
-
-    file->write(str.toUtf8());
-    file->close();
+    while( !in.atEnd() ) {
+        QString line = in.readLine();
+        qDebug() << line << endl;       // 이곳에 ui 안에 테이블위젯 안에 아이템중 row들을 하나씩 추가하여 .txt 파일에 있는 값들을 넣어주기.
+    }
+//    QString mText = in.readAll();
+    mFile.flush();
+    mFile.close();
 }
