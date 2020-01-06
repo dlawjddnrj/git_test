8d043d8117b71329ecad28d5f64907d72a3aa67a
JungWoo <dlawjddnrj01@github.com>
2020-01-06 16:46:47 +0900

    로또 번호 추출시 중복값 안나오게 코드 추가

--- Hyeonbin/LTrecommndPRG/fPj/myclass.cpp
+++ Hyeonbin/LTrecommndPRG/fPj/myclass.cpp
@@ -2,7 +2,7 @@
 
 #include <QDebug>
 #include <cstdlib>
-#include <ctime>
+#include <time.h>
 
 
 myClass::myClass(QObject* obj):
@@ -15,26 +15,29 @@ void myClass::onButtonClicked(QVariant var)
     int test = var.canConvert<int>() ? var.toInt() +1 : +0;
     int imsi = test;
     int test_arr[6];
-    int such = 0;
-    srand((unsigned int)time(NULL));
+    int temp;
+
+    srand((unsigned int) time (NULL));
 
     std::string sfd [] = {"a","b","c","d","e","f"};
+
     for (imsi = 0 ; imsi < 6 ; imsi ++) {
+        while(1) {
+            test_arr[imsi] = rand() % 45 + 1;
 
+            temp = 0;
 
-        test_arr[imsi] = {(rand()%45)+1};
-//        for (such ; such < imsi ; such++) {
-//            if(test_arr[imsi] == test_arr[such])
-//            imsi--;
-//            break;
-//        }
-
+            for(int j = 0; j < imsi; j++) {
+                if(test_arr[imsi] == test_arr[j]) {
+                    temp = 1;
+                    break;
+                }
+            }
+            if(!temp) {
+                break;
+            }
+        }
         qDebug() << test_arr[imsi] ;
-
         m_obj->setProperty(sfd[imsi].c_str(),QVariant(test_arr[imsi]));
-
-
     }
-
-
 }
